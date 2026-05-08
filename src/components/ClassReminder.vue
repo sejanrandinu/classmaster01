cf8dce7c94e50503300bff8b85ba17ba50b5a498
Sejan Randinu <sejanrandinu01@gmail.com>
2026-05-08 12:16:26 +0530

    zgs

--- src/components/ClassReminder.vue
+++ src/components/ClassReminder.vue
@@ -109,7 +109,7 @@ const triggerNotification = async (cls, diff) => {
     message: messageText,
     caption: `${subjectName} | ${tutorName}`,
     position: 'top-right',
-    icon: 'notifications_active',
+    avatar: '/favicon.png',
     timeout: 15000,
     actions: actions
   })
@@ -117,7 +117,7 @@ const triggerNotification = async (cls, diff) => {
   if (window.Notification && Notification.permission === 'granted') {
     new Notification('Class Reminder', {
       body: messageText,
-      icon: '/favicon.svg'
+      icon: '/favicon.png'
     })
   }
 }
