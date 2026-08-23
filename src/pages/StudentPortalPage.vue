82c75396a336301ed00862cb90e15175ca93039b
Sejan Randinu <sejanrandinu01@gmail.com>
2026-08-23 19:17:14 +0530

    feat: implement student portal page and PWA manifest configuration

--- src/pages/StudentPortalPage.vue
+++ src/pages/StudentPortalPage.vue
@@ -1604,6 +1604,7 @@ const downloadCharacterCertificate = () => {
 .text-line-clamp-3 {
   display: -webkit-box;
   -webkit-line-clamp: 3;
+  line-clamp: 3;
   -webkit-box-orient: vertical;
   overflow: hidden;
 }
